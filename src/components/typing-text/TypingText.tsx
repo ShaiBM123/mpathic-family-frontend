32813a7a8ae9fe62404935dedbd36739103346fe
ShaiBM123 <shaibenmichael@gmail.com>
2024-12-18 16:25:36 +0200

    TypingText customized component , including support for scroll to top and bottom on typing begin or complete

---
+++ src/components/typing-text/TypingText.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+// import Typed from 'typed.js';
+import { ReactTyped } from "react-typed";
+// import { TypedOptions } from 'typed.js';
+import {
+  ChatMessage,
+  MessageContentType,
+} from "@chatscope/use-chat";
+
+import {
+  useChat, MessageStatus
+} from "@chatscope/use-chat";
+
+export type TypingTextProps = {
+  chatMsg: ChatMessage<MessageContentType>;
+  chatMsgContentToStrings: (c: Object) => Array<string>
+  onStringTyped?: () => void
+  onComplete?: () => void
+}
+
+export const TypingText = ({ chatMsg, chatMsgContentToStrings, onStringTyped, onComplete }: TypingTextProps) => {
+
+  const { updateMessage } = useChat();
+  const doOnComplete = onComplete ? onComplete :
+    () => {
+      chatMsg.status = MessageStatus.Sent;
+      updateMessage(chatMsg)
+    };
+
+  return (
+    <ReactTyped
+      typeSpeed={20}
+      showCursor={true}
+      onStringTyped={onStringTyped}
+      onComplete={doOnComplete}
+      strings={chatMsgContentToStrings(chatMsg.content)}
+    />
+  );
+}
